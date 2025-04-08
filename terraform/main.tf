2fa90db3067c8003e5a3431c1220af9b44fbd66d
harry1321 <48749818+harry1321@users.noreply.github.com>
2025-04-08 10:53:26 +0800

    Testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,5 +48,5 @@ output "jupyter_url" {
 
 output "spark_url" {
   value       = "http://${google_compute_instance.airflow_vm.network_interface.0.access_config.0.nat_ip}:8080"
-  description = "Spark UI 的訪問網址"
+  description = "Airflow UI 的訪問網址"
 }
