ad9488341b88b03c23b21126d64d4ff6f7a59365
harry1321 <48749818+harry1321@users.noreply.github.com>
2025-04-09 17:38:00 +0800

    Update raw_to_silver.py

--- spark/raw_to_silver.py
+++ spark/raw_to_silver.py
@@ -45,14 +45,14 @@ conf = SparkConf() \
     .setAppName('test') \
     .set("spark.jars", "./lib/gcs-connector-hadoop3-2.2.5.jar") \
     .set("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
-    .set("spark.hadoop.google.cloud.auth.service.account.json.keyfile", credentials_path)
+    .set("spark.hadoop.google.cloud.auth.service.account.json.keyfile", str(credentials_path))
 sc = SparkContext(conf=conf)
 
 hadoop_conf = sc._jsc.hadoopConfiguration()
 
 hadoop_conf.set("fs.AbstractFileSystem.gs.impl",  "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
 hadoop_conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
-hadoop_conf.set("fs.gs.auth.service.account.json.keyfile", credentials_path)
+hadoop_conf.set("fs.gs.auth.service.account.json.keyfile", str(credentials_path))
 hadoop_conf.set("fs.gs.auth.service.account.enable", "true")
 
 # 初始化 SparkSession
