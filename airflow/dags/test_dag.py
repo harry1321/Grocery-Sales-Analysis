9a62e45eeb5b4b71d3cfc1da20dc3ed9a30ed0ce
harry1321 <48749818+harry1321@users.noreply.github.com>
2025-04-14 12:00:30 +0800

    Update test_dag.py

--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -39,6 +39,8 @@ def task_get(ti, dataset_name='andrexibiza/grocery-sales-dataset') -> None:
     api.dataset_download_files(dataset_name, path=folder_path, unzip=True)
     print("Kaggle 資料集下載完成！")
     file_names = os.listdir(folder_path)
+    for f in file_names:
+        print(file_names)
     file_names = [f for f in file_names if os.path.isfile(os.path.join(folder_path, f))]
     return {'check_list': file_names}
 
