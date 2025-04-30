89c59f09cee1b58f0bdf5dc3b2c851bd966475c7
harry1321 <48749818+harry1321@users.noreply.github.com>
2025-04-30 14:41:27 +0800

    Update batch_spark.py

--- airflow/dags/batch_spark.py
+++ airflow/dags/batch_spark.py
@@ -53,7 +53,6 @@ with dag:
         num_executors=2,
         livy_conn_id="livy",  # 在 Airflow Connections 設定你的 Livy endpoint
         polling_interval=30,
-        timeout=1800,
         deferrable=True  # 這裡是重點：啟用 deferrable mode
     )
 
@@ -79,7 +78,7 @@ with dag:
         livy_conn_id="livy",  # 在 Airflow Connections 設定你的 Livy endpoint
         polling_interval=30,
         timeout=1800,
-        deferrable=True  # 這裡是重點：啟用 deferrable mode
+        deferrable=True
     )
 
 date >> spark_raw_silver >> spark_silver_gold
